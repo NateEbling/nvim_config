2ca94419b0069f793e9a7cfc519387addf059eb7
Nate <ebling.nathan@gmail.com>
2024-05-24 11:05:11 -0400

    Updated theme

--- lua/nate/lazy/colors.lua
+++ lua/nate/lazy/colors.lua
@@ -1,22 +1,36 @@
-function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
 return {
-    'rose-pine/neovim',
-    name = 'rose-pine',
+   'olimorris/onedarkpro.nvim',
+    name = 'onedarkpro',
     config = function()
-        require('rose-pine').setup({
+        require('onedarkpro').setup({
             styles = {
-                italic = false
+                operators = "NONE",
+            },
+            highlights = {
+                ["@function"] = { fg = "#abb2bf"},
+                ["@variable"] = { fg = "#abb2bf"},
+                ["@type.builtin.c"] = { fg = "#e5c07b"},
+                ["@operator"] = { fg = "#abb2bf"},
+                ["@punctuation.bracket"] = { fg = "#abb2bf"},
+                ["@keyword.import.c"] = { fg = "#61afef"},
+                ["@function.call.c"] = { fg = "#abb2bf"},
+                ["@lsp.typemod.function.defaultLibrary.c"] = { fg = "#abb2bf"},
+                ["@string.escape.c"] = { fg = "#d19a66"},
+                ["@string"] = { fg = "#89ca78"},
+                ["@type.definition.c"] = { fg = "#abb2bf"},
+                ["@keyword.type.c"] = { fg = "#e5c07b"},
+                ["@property"] = { fg = "#abb2bf"},
+                ["@lsp.type.macro.c"] = { fg = "#d55fde"},
+                ["@keyword.directive.define.c"] = { fg = "#d55fde"},
+                ["@keyword.return.c"] = { fg = "#d55fde"},
+                ["@keyword.conditional.c"] = { fg = "#d55fde"},
+                ["@keyword.repeat.c"] = { fg = "#d55fde"},
+                ["@keyword.c"] = { fg = "#d55fde"},
+                ["@variable.parameter.c"] = { fg = "#abb2bf"},
             }
         })
 
-        vim.cmd('colorscheme rose-pine')
+        vim.cmd('colorscheme onedark')
 
         end
 }
