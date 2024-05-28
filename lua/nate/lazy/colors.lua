9261379aa09b39706eabea4775a4bdd3ddff1eea
NateEbling <ebling.nathan@gmail.com>
2024-05-27 22:59:20 -0400

    Updated colors

--- lua/nate/lazy/colors.lua
+++ lua/nate/lazy/colors.lua
@@ -1,4 +1,14 @@
+function ColorMyPencils(color)
+    color = color or "rose-pine"
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
 return {
+    {
    'olimorris/onedarkpro.nvim',
     name = 'onedarkpro',
     config = function()
@@ -36,4 +46,22 @@ return {
         vim.cmd('colorscheme onedark')
 
         end
-    }
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require('rose-pine').setup({
+                disable_background = true,
+                styles = {
+                    italic = false,
+                },
+            })
+
+            vim.cmd("colorscheme rose-pine")
+
+            ColorMyPencils()
+        end
+    },
+
+}
