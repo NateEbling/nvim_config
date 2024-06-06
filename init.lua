e7b657a8092c5ba773a1fd9af8f94020f8902968
NateEbling <ebling.nathan@gmail.com>
2024-06-06 12:10:55 -0400

    Added vimrc.vim as an init file to set a vim colorscheme (paper)

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 require("nate")
 
+local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
+vim.cmd.source(vimrc)
+
---
+++ vimrc.vim
@@ -0,0 +1,5 @@
+call plug#begin()
+Plug 'yorickpeterse/vim-paper'
+call plug#end()
+
+colorscheme paper
