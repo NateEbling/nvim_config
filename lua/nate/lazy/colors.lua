d95e389eb70274c5e83c5cc50d2f34a3a89e3102
NateEbling <ebling.nathan@gmail.com>
2024-06-29 20:05:39 -0400

    .

--- lua/nate/lazy/colors.lua
+++ lua/nate/lazy/colors.lua
@@ -5,8 +5,16 @@ return {
       priority = 1000,
       config = function()
         vim.g.gruvbox_material_enable_italic = false
-        vim.cmd.colorscheme('gruvbox-material')
+        --vim.cmd.colorscheme('gruvbox-material')
         vim.g.gruvbox_material_foreground = 'mix'
       end
+    },
+    { 
+    	"ellisonleao/gruvbox.nvim", 
+    	lazy = false,
+    	priority = 1000, 
+    	config = function()
+    	  vim.cmd.colorscheme('gruvbox')
+    	end
     }
 }
