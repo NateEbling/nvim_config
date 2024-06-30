c7f4d64c351f4b456e1d6ceeed3dc8e7bcef0a23
NateEbling <ebling.nathan@gmail.com>
2024-06-29 20:22:55 -0400

    Configuring gruvbox

--- lua/nate/lazy/colors.lua
+++ lua/nate/lazy/colors.lua
@@ -12,9 +12,21 @@ return {
     { 
     	"ellisonleao/gruvbox.nvim", 
     	lazy = false,
-    	priority = 1000, 
+    	priority = 1000,
     	config = function()
+    	  require("gruvbox").setup({
+    	    overrides = {
+    	      ["@punctuation.bracket"] = { fg = "#bdbdbd" },
+    	      ["@punctuation.delimiter"] = { fg = "#bdbdbd" },
+    	      ["@operator"] = { fg = "#bdbdbd" },
+    	      ["@variable.parameter"] = { fg = "#bdbdbd" },
+    	      ["@lsp.type.parameter.cpp"] = { fg = "#bdbdbd" },
+    	      ["@lsp.type.property.cpp"] = { fg = "#bdbdbd" },
+    	      ["@keyword.directive.cpp"] = { fg = "#ebebeb" },
+    	      ["@keyword.directive.define.cpp"] = { fg = "#ebebeb" },
+    	    }
+    	  })
     	  vim.cmd.colorscheme('gruvbox')
-    	end
+    	 end
     }
 }
